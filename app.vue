c035817590f8140e090a59a710a753399d0b9c43
QuantumParrot <ataraxia528491@gmail.com>
2024-11-05 17:56:59 +0800

    每日任務 - 2024.11.04

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+const route = useRoute();
+
+</script>
+
+<template>
+<div>
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
+    <p>當前的路由路徑 ( <code v-pre>route.fullPath</code> ) 為：{{ route.fullPath }}</p>
+</div>
+</template>
