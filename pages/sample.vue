835b95c09d79c48669a83ae836546ef9699dc001
QuantumParrot <ataraxia528491@gmail.com>
2024-11-07 18:04:41 +0800

    fix: 補上漏掉的檔案 / 每日任務 - 2024.11.05

---
+++ pages/sample.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+
+<h1 class="title mb-3">示範</h1>
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+<button type="button" class="btn btn-link">Link</button>
+
+</template>
+
+<style lang="scss">
+
+.title { color: $test; }
+
+</style>
