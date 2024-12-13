323a0438eac4e5594042fb6a7a7cf0c3757d582b
QuantumParrot <ataraxia528491@gmail.com>
2024-12-13 19:14:30 +0800

    每日任務 - 11.25

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,22 @@
-<script setup></script>
+<script setup>
+
+const { $modal, $offcanvas } = useNuxtApp();
+
+const modal = ref(null);
+let modalController;
+
+const offcanvas = ref(null);
+let offcanvasController;
+
+onMounted(() => {
+
+    modalController = $modal(modal.value);
+
+    offcanvasController = $offcanvas(offcanvas.value, { backdrop: 'static' });
+
+});
+
+</script>
 
 <template>
 
@@ -18,6 +36,62 @@
 請點選 Navbar 的 “ 選擇房型 ”
 <hr>
 🌿 <NuxtLink to="/register">每日任務 DAY 09 - $fetch 與 ofetch</NuxtLink>
+<hr>
+<p>🌿 每日任務 DAY 16</p>
+<button
+    type="button" class="btn btn-primary me-2"
+    @click="modalController.show">
+    Launch Demo Modal
+</button>
+<button
+    type="button" class="btn btn-secondary" 
+    @click="offcanvasController.show">
+    Toggle Static Offcanvas
+</button>
+
+</div>
+
+<!-- Modal -->
+
+<div
+    ref="modal"
+    class="modal fade"
+    tabindex="-1" aria-labelledby="modal-label" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title fs-5" id="modal-label">Modal Title</h5>
+                <button
+                    type="button" class="btn-close"
+                    aria-label="Close" @click="modalController.hide">
+                </button>
+            </div>
+            <div class="modal-body">
+                <p class="mb-0">Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" @click="modalController.hide">關閉</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Offcanvas -->
+
+<div
+    ref="offcanvas"
+    class="offcanvas offcanvas-start"
+    tabindex="-1" aria-labelledby="static-backdrop-label">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title fs-5" id="static-backdrop-label">Offcanvas</h5>
+        <button
+            type="button" class="btn-close"
+            aria-label="Close" @click="offcanvasController.hide">
+        </button>
+    </div>
+    <div class="offcanvas-body">
+        <div>I will not close if you click outside of me.</div>
+    </div>
 </div>
 
 </template>
--- plugins/bootstrap.client.js
+++ plugins/bootstrap.client.js
@@ -1,7 +1,20 @@
-import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
+// import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
+
+import { Modal, Offcanvas } from 'bootstrap/dist/js/bootstrap.bundle.min';
 
 export default defineNuxtPlugin((nuxtApp) => {
 
-    nuxtApp.provide("bootstrap", bootstrap);
+    // nuxtApp.provide("bootstrap", bootstrap);
+
+    return {
+
+        provide: {
+
+            modal(element, options) { return new Modal(element, options) },
+            offcanvas(element, options) { return new Offcanvas(element, options) }
+
+        }
+
+    }
 
 });
